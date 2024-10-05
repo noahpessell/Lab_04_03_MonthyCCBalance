f9964a54ac8f20b53b3a82d0d40e900ede98fd9b
noahpessell <nwpessell@mail.com>
2024-10-04 23:54:59 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//Start
+//	OUTPUT “What is your credit card balance?”
+//	INPUT creditBalance
+//	INTEREST_RATE = 0.17
+//	creditInterest = creditBalance * INTEREST_RATE
+//	OUTPUT “The interest accrued on your credit card balance after
+//one month is ” + creditInterest
+//creditBalance = creditBlanace + creditInterest
+//creditInterest = creditInterest + (creditBalance *
+//INTEREST_RATE)
+//OUTPUT “The interest accrued on your credit card balance after
+//two months is “ + creditInterest
+//Stop
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double creditBalance = 5000.00;
+        double interestRate = 0.17;
+        double creditInterest = creditBalance * interestRate;
+        System.out.println("Your interest due after one month is: $" + creditInterest);
+        creditBalance= creditBalance + creditInterest;
+        creditInterest = creditBalance * interestRate;
+        System.out.println("Your interest due after two months is: $" + creditInterest);
+    }
+}
